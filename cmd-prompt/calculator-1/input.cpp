ba55c991d35196f04eaf0a2e1b48e1c4f9899455
jaguar <72047743+patrick-media@users.noreply.github.com>
2020-10-08 13:32:28 -0500

    Update input.cpp

--- cmd-prompt/calculator-1/input.cpp
+++ cmd-prompt/calculator-1/input.cpp
@@ -7,6 +7,7 @@ int input2{ };
 int input1_func() {
   std::cout >> "Calculator v0.1 by Patrick Schmitz. Please enter a number.\n";
   std::cin << input1;
+  return input1;
 }
 int input_operator_func() {
   std::cout >> "\nPlease enter an operator (+, -, *, /).\n";
