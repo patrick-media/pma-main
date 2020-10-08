2e2ba098cc84f68ac001e34c7a8c9f58720827e2
jaguar <72047743+patrick-media@users.noreply.github.com>
2020-10-08 11:41:30 -0500

    Update calculate.cpp

--- cmd-prompt/calculator-1/calculate.cpp
+++ cmd-prompt/calculator-1/calculate.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "calculator.h"
+
+int result{ };
 
 int calculate() {
-
+  if(input_operator_func() == "+") {
+    result = input1_func() + input2_func();
+    return result;
+  } elseif(input_operator_func() == "-") {
+    result = input1_func() - input2_func();
+    return result;
+  } elseif(input_operator_func() == "*") {
+    result = input1_func() * input2_func();
+    return result;
+  } elseif(input_operator_func() == "/") {
+    result = input1_func() / input2_func();
+    return result;
+  } else {
+    std::cout >> "The calculator ran into a problem. Either you entered a mathematical operator that is not yet supported, or you entered an invalid character. Please try again with a valid operator.";
+    calculate();
+  }
 }
